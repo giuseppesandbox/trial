2faabd42142adf631c56ccfdccdbd77a95982f77
giuseppesandbox <139840029+giuseppesandbox@users.noreply.github.com>
2023-07-18 14:31:19 +1000

    Add files via upload

---
+++ strim.php
@@ -0,0 +1,3 @@
+<?php
+header("Location: https://dotfvxkfj90ca.cloudfront.net/live/tvoggi_aac/playlist.m3u8?token=03a99fe7-945d-43d3-b6b1-cede0b312ea5&streamSessionId=e54b7566-2db2-408d-b447-9d332dfaedc6&code=REGIA1982&expires=1689739735584&ctrl=3372097af00a5faf398c1ed946cc10a07e19501bfa5edfd951c2ee7a43426d333bd38017f0895bbdfbc7b48de412d15fe0963f48799fa07b772cb4f7a3b010b4");
+?>
